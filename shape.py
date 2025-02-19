1081c478877cf1f9ef7f483ffcdb9caea71dc685
justatrade <justatrade@mail.ru>
2025-02-19 03:15:40 +0300

    Обновлённая версия shape с повышенным разрешением и выполняемыми ограничениями по размерам фигур

--- shape.py
+++ shape.py
@@ -3,8 +3,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 import matplotlib.pyplot as plt
 
-NUM_POINTS = 100
-
+NUM_POINTS = 1000
 
 class Shape(ABC):
     def __init__(self, name):
@@ -15,11 +14,7 @@ class Shape(ABC):
     def generate_reference(self):
         pass
 
-    @abstractmethod
     def save_to_json(self):
-        pass
-
-    def print_json(self):
         data = {
             "shape": self.name,
             "points": self.points.tolist() if isinstance(self.points, np.ndarray) else self.points
@@ -27,11 +22,8 @@ class Shape(ABC):
         print(json.dumps(data, indent=4))
 
     def plot(self, filename=None):
-        """
-        Отрисовывает фигуру с помощью matplotlib и сохраняет в файл.
-        """
-        plt.figure(figsize=(6, 6))  # Задаём квадратное изображение 6x6 дюймов
-        plt.scatter(self.points[:, 0], self.points[:, 1], label=self.name, s=10)  # Уменьшаем размер точек (s=10)
+        plt.figure(figsize=(6, 6))
+        plt.scatter(self.points[:, 0], self.points[:, 1], label=self.name, s=10)
         plt.title(self.name)
         plt.grid(True)
         plt.axhline(0, color='black', linewidth=0.5)
@@ -39,77 +31,41 @@ class Shape(ABC):
         plt.xlabel("X")
         plt.ylabel("Y")
         plt.legend()
-
-        # Устанавливаем одинаковый масштаб по осям X и Y
         plt.gca().set_aspect('equal', adjustable='box')
-
         if filename:
-            plt.savefig(filename, dpi=100)  # Сохраняем с разрешением 100 dpi
+            plt.savefig(filename, dpi=200)
         else:
-            plt.savefig(f"{self.name}.png", dpi=100)  # Сохраняем с разрешением 100 dpi
+            plt.savefig(f"{self.name}.png", dpi=200)
         plt.close()
 
-
 class Square(Shape):
-    def __init__(self, side_length=10):
+    def __init__(self, side_length=20):
         super().__init__("square")
         self.side_length = side_length
 
     def generate_reference(self, num_points=NUM_POINTS):
         points_per_side = num_points // 4
-
-        bottom_points = [(x, -self.side_length / 2) for x in
-                         np.linspace(-self.side_length / 2, self.side_length / 2, points_per_side)]
-
-        right_points = [
-            (self.side_length / 2, y) for y in
-            np.linspace(-self.side_length / 2, self.side_length / 2, points_per_side)
-        ]
-        top_points = [
-            (x, self.side_length / 2) for x in
-            np.linspace(self.side_length / 2, -self.side_length / 2, points_per_side)
-        ]
-        left_points = [
-            (-self.side_length / 2, y) for y in
-            np.linspace(self.side_length / 2, -self.side_length / 2, points_per_side)
-        ]
-
+        bottom_points = [(x, -self.side_length / 2) for x in np.linspace(-self.side_length / 2, self.side_length / 2, points_per_side)]
+        right_points = [(self.side_length / 2, y) for y in np.linspace(-self.side_length / 2, self.side_length / 2, points_per_side)]
+        top_points = [(x, self.side_length / 2) for x in np.linspace(self.side_length / 2, -self.side_length / 2, points_per_side)]
+        left_points = [(-self.side_length / 2, y) for y in np.linspace(self.side_length / 2, -self.side_length / 2, points_per_side)]
         self.points = np.array(bottom_points + right_points + top_points + left_points)
 
-    def save_to_json(self):
-        self.print_json()
-
-
 class Triangle(Shape):
-    def __init__(self, side_length=10):
+    def __init__(self, side_length=20):
         super().__init__("triangle")
         self.side_length = side_length
 
     def generate_reference(self, num_points=NUM_POINTS):
         points_per_side = num_points // 3
         height = (np.sqrt(3) / 2 * self.side_length)
-
-        base_points = [(x, -height / 3) for x in
-                       np.linspace(-self.side_length / 2, self.side_length / 2, points_per_side)]
-
-        right_points = [(x, y) for x, y in zip(
-            np.linspace(self.side_length / 2, 0, points_per_side),
-            np.linspace(-height / 3, 2 * height / 3, points_per_side)
-        )]
-
-        left_points = [(x, y) for x, y in zip(
-            np.linspace(-self.side_length / 2, 0, points_per_side),
-            np.linspace(-height / 3, 2 * height / 3, points_per_side)
-        )]
-
+        base_points = [(x, -height / 3) for x in np.linspace(-self.side_length / 2, self.side_length / 2, points_per_side)]
+        right_points = [(x, y) for x, y in zip(np.linspace(self.side_length / 2, 0, points_per_side), np.linspace(-height / 3, 2 * height / 3, points_per_side))]
+        left_points = [(x, y) for x, y in zip(np.linspace(-self.side_length / 2, 0, points_per_side), np.linspace(-height / 3, 2 * height / 3, points_per_side))]
         self.points = np.array(base_points + right_points + left_points)
 
-    def save_to_json(self):
-        self.print_json()
-
-
 class Circle(Shape):
-    def __init__(self, radius=5):
+    def __init__(self, radius=10):
         super().__init__("circle")
         self.radius = radius
 
@@ -117,59 +73,33 @@ class Circle(Shape):
         angles = np.arange(0, 2 * np.pi, 2 * np.pi / num_points)
         self.points = np.array([(self.radius * np.cos(angle), self.radius * np.sin(angle)) for angle in angles])
 
-    def save_to_json(self):
-        self.print_json()
-
-
 class Parallelogram(Shape):
-    def __init__(self, base=10, height=5, skew=2):
+    def __init__(self, base=16, height=10, skew=4):
         super().__init__("parallelogram")
         self.base = base
         self.height = height
         self.skew = skew
 
     def generate_reference(self, num_points=NUM_POINTS):
-        # TODO: В идеале, рассчитывать количество точек пропорционально итоговой величине стороны
         points_per_side = num_points // 4
-
         bottom_left = (-self.base - self.skew) / 2
         bottom_right = (self.base - self.skew) / 2
         top_left = -(self.base - self.skew) / 2
         top_right = (self.base + self.skew) / 2
-        base_points = [
-            (x, -self.height / 2) for x in np.linspace(
-                bottom_left, bottom_right, points_per_side
-            )
-        ]
-        top_points = [
-            (x, self.height / 2) for x in np.linspace(
-                top_left, top_right, points_per_side
-            )
-        ]
-        left_points = [(bottom_left + self.skew * t, y) for t, y in zip(
-            np.linspace(0, 1, points_per_side),
-            np.linspace(-self.height / 2, self.height / 2, points_per_side)
-        )]
-        right_points = [(bottom_right + self.skew * t, y) for t, y in zip(
-            np.linspace(0, 1, points_per_side),
-            np.linspace(-self.height / 2, self.height / 2, points_per_side)
-        )]
-
+        base_points = [(x, -self.height / 2) for x in np.linspace(bottom_left, bottom_right, points_per_side)]
+        top_points = [(x, self.height / 2) for x in np.linspace(top_left, top_right, points_per_side)]
+        left_points = [(bottom_left + self.skew * t, y) for t, y in zip(np.linspace(0, 1, points_per_side), np.linspace(-self.height / 2, self.height / 2, points_per_side))]
+        right_points = [(bottom_right + self.skew * t, y) for t, y in zip(np.linspace(0, 1, points_per_side), np.linspace(-self.height / 2, self.height / 2, points_per_side))]
         self.points = np.array(base_points + top_points + left_points + right_points)
-
-    def save_to_json(self):
-        self.print_json()
-
 
 if __name__ == "__main__":
     shapes = [
-        Square(side_length=10),
-        Triangle(side_length=10),
-        Circle(radius=5),
-        Parallelogram(base=10, height=5, skew=2)
+        Square(side_length=20),
+        Triangle(side_length=20),
+        Circle(radius=10),
+        Parallelogram(base=16, height=10, skew=4)
     ]
-
     for shape in shapes:
         shape.generate_reference()
-        shape.save_to_json()
+        # shape.save_to_json()
         shape.plot()
